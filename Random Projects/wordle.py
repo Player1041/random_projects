9aa000b6758339f9526863cc8b7d2a10df90ddb2
Player1041 <voidlessrift@outlook.com>
2022-02-22 22:00:17 +0000

    yyyyyyeah course

---
+++ Random Projects/wordle.py
@@ -0,0 +1,40 @@
+import random
+split = "--------------------------"
+
+4_letters = ["spin",
+"push",
+"side",
+"stop",
+"hurl",
+"feed",
+"fade",
+"lead",
+"mail",
+"seat",
+"wire",
+"cave",
+"tent",
+"duty",
+"game",
+"soap",
+"blow",
+"bear",
+"slab",
+"deny",
+"fist",
+"poem",
+"full",
+"meat",
+]
+
+
+
+print("Welcome to Wordle MK2 by Sean!")
+print(split)
+print("Please select your word length using the numbers on the left:\n")
+print("1 | 4 Letters.")
+print("2 | 5 Letters.")
+print("3 | 6 Letters.\n")
+print(split)
+choice = input()
+
