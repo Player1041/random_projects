5c06cd49243f0affa5cd8d7c20e0ee04f1ef83e2
Player1041 <voidlessrift@outlook.com>
2022-02-23 20:56:50 +0000

    wordle works :)

--- wordle.py
+++ wordle.py
@@ -1,7 +1,12 @@
 import random
+import time
 split = "--------------------------"
-
-four_letters = ["spin",
+red = "\u001b[31m"
+green = "\u001b[32m"
+white = "\u001b[37;1m"
+reset = "\u001b[0m"
+four_letters = [
+"spin",
 "push",
 "side",
 "stop",
@@ -29,11 +34,58 @@ four_letters = ["spin",
 "crop",
 "mask",
 "open",
-"file"]
+"file",
+"pest",
+"quit",
+"heir",
+"thaw",
+"main",
+"duck",
+"door",
+"weed",
+"swim",
+"care",
+"play",
+"bend",
+"wing",
+"pool",
+"soft",
+"deer",
+"rape",
+"nose",
+"grip",
+"sign",
+"park",
+"harm",
+"fear",
+"fall",
+"slam",
+"node",
+"plug",
+"ring",
+"slow",
+"dorm",
+"mile",
+"rain",
+"veil",
+"film",
+"hurl",
+"dash",
+"boot",
+"gasp",
+"hike",
+"gold",
+"golf",
+"bear",
+"bean",
+"burn",
+"like",
+"look"
+]
 
 
 
-final_fours = list(dict.fromkeys(four_letters))
+final_fours = list(set(four_letters))
 play = True
 while play == True:
     print("Welcome to Wordle MK2 by Sean!")
@@ -48,7 +100,7 @@ while play == True:
     if choice == 1:
       word = random.choice(final_fours)
       total = 4
-    """,elif choice == 2:
+    """elif choice == 2:
       word = random.choice(final_fives)
       total = 5
     elif choice == 3:
@@ -56,19 +108,29 @@ while play == True:
       total = 6"""
     
     guesses = 0
-    underscores = ['_' for _ in range(total)]
-    
-    while guesses < 6:
-      guess = input(f"Please guess the word: {underscores}")
+    underscores = ['_ ' for _ in range(total)]
+    while guesses < 5:
+      guess = input(f"Please guess the word: {''.join(underscores)} ")
       for count, value in enumerate(guess):
-        if value in word:
-            underscores[count] = value
+        if value in word[count]:
+          underscores[count] = value
       guesses = guesses + 1
-      if guess = word:
+      if guess == word:
         break
-      
-    print(f"Congrats! The word was {word}. It took you {guesses} of 5 guesses to get right. Do you wish to play again?")
+
+    print(split)
+    if guess == word:
+      print(f"Congrats! The word was {word}. It took you {guesses} of 5 guesses to get right.")
+    elif guess != word:
+      print(f"Sorry, you didn't get the word. The word was {word}.")
+    print("Do you wish to play again?")
     print("1 | Yes \n2 | No")
     play = int(input())
+    if play == 1:
+      play = True
+      print(f"\nRestarting script...\n{split}\n\n\n\n")
+    elif play == 2:
+      play = False
 print("Ok then. Bye!")
 print("--- END OF FILE ---")
+time.sleep(5)
